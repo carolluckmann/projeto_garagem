4906f8c20e87557f999a495ca01107f48f448c07
carolluckmann <carolinaluckmann05@gmail.com>
2024-04-01 14:35:40 -0300

    Adicionando id em todos os returns das API's

--- core/models/cor.py
+++ core/models/cor.py
@@ -2,5 +2,8 @@ from django.db import models
 
 class Cor(models.Model):
     nome = models.CharField(max_length=100)
+    class Meta:
+        verbose_name = "Cor"
+        verbose_name_plural = "Cores"
     def __str__(self):
-        return self.nome
+        return f"{self.nome} ({self.id})"
