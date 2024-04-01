62a2e6a306936ac31d060d7f1672aa7954a1e204
carolluckmann <carolinaluckmann05@gmail.com>
2024-04-01 14:32:04 -0300

    Criação da API para Marca

--- app/urls.py
+++ app/urls.py
@@ -13,11 +13,13 @@ from core.views import UserViewSet
 from core.views import AcessorioViewSet 
 from core.views import CategoriaViewSet
 from core.views import CorViewSet
+from core.views import MarcaViewSet
 
 router = DefaultRouter()
 router.register(r"acessorios", AcessorioViewSet, basename="acessorios")
 router.register(r"categorias", CategoriaViewSet, basename="categorias")
-router.register(r"cores", CorViewSet, basename="cores") 
+router.register(r"cores", CorViewSet, basename="cores")
+router.register(r"marcas", MarcaViewSet, basename="marcas") 
 router.register(r"users", UserViewSet, basename="users")
 
 urlpatterns = [
--- core/models/marca.py
+++ core/models/marca.py
@@ -4,4 +4,4 @@ class Marca(models.Model):
     nome = models.CharField(max_length=50)
     nacionalidade = models.CharField(max_length=50, null=True)
     def __str__(self):
-        return self.nome.upper()
+        return f"{self.nome.upper()} ({self.id})"
